cdd46f81f2bcb550d51b6fe77bd086b7428b2b81
Firere <67512865+Firere@users.noreply.github.com>
2024-08-13 21:35:32 +0200

    perf: optimise imports

--- merge.py
+++ merge.py
@@ -1,11 +1,11 @@
-import json
+from json import dumps, loads
 
 with open("legacy.json", "r", encoding="utf-8") as clips_old:
     with open("clipsmerged.json", "r", encoding="utf-8") as clips_new:
         with open("clipsmergedlegacy.json", "w", encoding="utf-8") as merged:
-            clips_old = json.loads(clips_old.read())
-            clips_new = json.loads(clips_new.read())
+            clips_old = loads(clips_old.read())
+            clips_new = loads(clips_new.read())
             for clip in clips_new:
                 clips_old[clip] = clips_new[clip]
 
-            merged.write(json.dumps(clips_old))
+            merged.write(dumps(clips_old))
